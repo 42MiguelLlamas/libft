9c069dc82a7e4e3ef4bf15fc05b9bd3b85074afe
Miguel Llamas Saez <mllamas-@c2r15s3.42madrid.com>
2023-10-03 19:46:03 +0200

    va_arg

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mllamas- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 18:01:50 by mllamas-          #+#    #+#             */
-/*   Updated: 2023/10/02 12:38:21 by mllamas-         ###   ########.fr       */
+/*   Updated: 2023/10/03 19:45:51 by mllamas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdarg.h>
@@ -18,9 +18,29 @@
 int	ft_printf(char const *format, ...)
 {
 	va_list args;
-
-	if (ft_checkformat(format) == 1);
-		return (1);
+	
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			if (*(format + 1) == 'c')
+				write(1, va_arg(args, char), 1);
+			if (*(format + 1) == 's')
+	
+			if (*(format + 1) == 'p')
+			if (*(format + 1) == 'd')
+			if (*(format + 1) == 'i')
+			if (*(format + 1) == 'u')
+			if (*(format + 1) == 'x')
+			if (*(format + 1) == 'X')
+			if (*(format + 1) == '%')
+				write(1, *(format + 1), 1);
+			format++;
+		else
+			write(1, *format, 1);
+		}
+	}
 	return (0);
 }
 
